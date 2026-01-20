7bcc4aabb093076d366604feef861bd42a8c4a34
Nikhil Kumar <alkaofshalimpur@gmail.com>
2026-01-20 22:31:59 +0530

    Add files via upload

--- 04.c
+++ 04.c
@@ -1,5 +1,15 @@
-
 #include <stdio.h>
-int main(void){
-  return 0;
+
+
+void main(){
+    long long int user_num;
+    printf("\t================ Even Or Odd ==================\n");
+    printf("\n\tEnter a number to find it's odd or not: ");
+    scanf("%d",&user_num);
+    if(user_num%2==0){
+        printf("\n\t%d is an Even Number.",user_num);
+
+    }else{
+        printf("\n\t%d is an Odd Number.",user_num);
+    };
 }
