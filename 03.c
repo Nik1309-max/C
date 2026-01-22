ba0c39f0c9673c714d90b4a24cb56503d79d65dd
Nikhil Kumar <alkaofshalimpur@gmail.com>
2026-01-22 14:24:32 +0530

    Add files via upload

--- 03.c
+++ 03.c
@@ -1,4 +1,20 @@
-#inclue <stdio.h>
-int main(void){
-  return 0;
+#include <stdio.h>
+int main(){
+    int a,b,c;
+    printf("\n\t============== Palindrome Checker==============\n");
+    printf("\n\tEnter a number to find whether it's Palindrome or not: ");
+    scanf("%d",&a);
+    b = a;
+    c = 0;
+    while(b){
+        c = c*10+(b%10);
+        b/=10;
+    };
+    if(a==c){
+        printf("\n\t%d is a Palindrome Number.\n",a);
+    }else{
+        printf("\n\t%d is not a Palindrome Number.\n",a);
+    }
+    return 0;
 }
+
