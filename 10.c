507c12938f8f6f97a1bdd1b0e586084abb171a91
Nikhil Kumar <alkaofshalimpur@gmail.com>
2026-01-27 21:38:05 +0530

    Add files via upload

---
+++ 10.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+    double a,b;
+    short c;
+    printf("Enter the first number: ");
+    scanf("%d",&a);
+    printf("Enter the second number: ");
+    scanf("%d",&a);
+    printf("Choose Addition | Multiplication | Division | sub: ")
+    return;
+}
