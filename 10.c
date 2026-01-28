7f87e2b2ca64bae34a6feb8f8a51a6526305bfb9
Nikhil Kumar <alkaofshalimpur@gmail.com>
2026-01-28 22:30:21 +0530

    Add files via upload

--- 10.c
+++ 10.c
@@ -1,11 +1,49 @@
 #include<stdio.h>
+void addition(int a, int b);
+void multiplication(int a, int b);
+void divison(int a, int b);
+void subtraction(int a, int b);
+
 void main(){
-    double a,b;
-    short c;
+    int a,b;
+    int c;
     printf("Enter the first number: ");
     scanf("%d",&a);
     printf("Enter the second number: ");
-    scanf("%d",&a);
-    printf("Choose Addition | Multiplication | Division | sub: ")
+    scanf("%d",&b);
+    printf("Choose \n1. Addition\n2. Multiplication \n3. Division \n4. Subtraction: ");
+    scanf("%d",&c);
+    switch (c){
+        case 1: 
+            addition(a,b);
+            break;
+        case 2: 
+            multiplication(a,b); 
+            break;
+        case 3: 
+            divison(a,b);
+            break;
+        case 4: 
+            subtraction(a,b);
+            break;
+        default: 
+            printf("Wrong input...");
+    }
+        
     return;
+}
+
+void addition(int a, int b){
+    printf("The addition of %d and %d is: %d",a,b,a+b); 
+
+}
+void multiplication(int a, int b){
+    printf("The multiplication of %d and %d is: %d",a,b,a*b);
+}
+void divison(int a, int b){
+    printf("The division of %d and %d is: %d",a,b,a/b); 
+}
+void subtraction(int a, int b){
+    printf("The subtraction of %d and %d is: %d",a,b,a-b);
+
 }
